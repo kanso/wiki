5286d214ed70c14c873c67551fa7fce8b803abe9
Caolan McMahon <caolan@caolanmcmahon.com>
2012-01-23 16:41:09 -0800

    update packages, add icons, screenshots and metadata

---
+++ packages/handlebars-helpers/build/helpers.js
@@ -0,0 +1,20 @@
+Handlebars.registerHelper('uc', function (str) {
+    return encodeURIComponent(str);
+});
+
+// TODO: add optional context argument?
+Handlebars.registerHelper('include', function (name) {
+    if (!exports.templates[name]) {
+        throw new Error('Template Not Found: ' + name);
+    }
+    return exports.templates[name](this, {});
+});
+
+Handlebars.registerHelper('ifequal', function (val1, val2, fn, elseFn) {
+    if (val1 === val2) {
+        return fn();
+    }
+    else if (elseFn) {
+        return elseFn();
+    }
+});
